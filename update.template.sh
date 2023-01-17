cb6138c02da32a98f0a29fa5cc16151bf0d60807
Oliver Lee <oliverzlee@gmail.com>
2023-01-16 17:30:35 -0800

    Format `cc_library` targets without files in `srcs`

--- update.template.sh
+++ update.template.sh
@@ -3,22 +3,30 @@ set -euo pipefail
 
 bazel=$(readlink -f /proc/${PPID}/exe)
 
+function stale()
+{
+echo "$1" \
+      | grep "ERROR: action 'ClangFormat" \
+      | sed "s/^ERROR: action 'ClangFormat \(.*\)\.clang_format' is not up-to-date$/\1/" || true
+}
+
+bazel_query=("$bazel" query \
+                      --color=yes \
+                      --noshow_progress \
+                      --ui_event_filters=-info)
+
 bazel_format=("$bazel" build \
                     --noshow_progress \
                     --ui_event_filters=-info,-stdout \
                     --color=no \
-                    --compile_one_dependency \
                     --aspects=@@WORKSPACE@//:defs.bzl%clang_format_aspect \
                     --@@WORKSPACE@//:binary=@BINARY@ \
                     --@@WORKSPACE@//:config=@CONFIG@ \
                     --output_groups=report)
 
+bazel_format_file=("${bazel_format[@]}" --compile_one_dependency)
+
 cd $BUILD_WORKSPACE_DIRECTORY
-
-args=$(printf " union %s" "${@}" | sed "s/^ union \(.*\)/\1/")
-
-sources=$("$bazel" query --color=yes --noshow_progress \
-    "let t = kind(\"cc_.* rule\", ${args:-//...}) in labels(srcs, \$t) union labels(hdrs, \$t)")
 
 # manually build wrapper to set up runfiles tree
 # https://stackoverflow.com/questions/48178323/during-bazel-build-when-is-target-runfiles-directory-properly-set-up
@@ -27,30 +35,47 @@ sources=$("$bazel" query --color=yes --noshow_progress \
          --ui_event_filters=-info,-stdout \
          @@WORKSPACE@//:wrapper &>/dev/null
 
-result=$("${bazel_format[@]}" \
+args=$(printf " union %s" "${@}" | sed "s/^ union \(.*\)/\1/")
+
+source_files=$("${bazel_query[@]}" \
+    "let t = kind(\"cc_.* rule\", ${args:-//...}) in labels(srcs, \$t) union labels(hdrs, \$t)")
+
+result=$("${bazel_format_file[@]}" \
              --keep_going \
              --check_up_to_date \
-             $sources 2>&1 || true)
+             $source_files 2>&1 || true)
 
-files=$(echo "$result" \
-    | grep "ERROR: action 'ClangFormat" \
-    | sed "s/^ERROR: action 'ClangFormat \(.*\)\.clang_format' is not up-to-date$/\1/" || true)
+files=$(stale "$result")
 
 if [[ -z $files ]] && [[ $(echo "$result" | grep "ERROR:" | wc -l) -gt 0 ]]; then
     echo "$result"
     exit 1
 fi
 
-count=$(echo "$files" | sed '/^\s*$/d' | wc -l)
-trap ">&2 echo \"Formatted $count file(s).\"" EXIT
+# libraries without `srcs` are not handled correctly with `--compile_one_dependency`
+header_libs=$("${bazel_query[@]}" \
+    "attr(\"srcs\", \"\[\]\", kind(\"cc_library rule\", ${args:-//...}))")
 
-[[ $count -ne 0 ]] || exit 0
+result=$("${bazel_format[@]}" \
+             --keep_going \
+             --check_up_to_date \
+             $header_libs 2>&1 || true)
+
+header_files=$(stale "$result")
+
+file_count=$(echo "$files" | sed '/^\s*$/d' | wc -l)
+header_file_count=$(echo "$header_files" | sed '/^\s*$/d' | wc -l)
+
+[[ $file_count -ne 0 ]] || [[ $header_file_count -ne 0 ]] || exit 0
 
 # use bazel to generate the formatted files in a separate
 # directory in case the user is overriding .clang-format
-"${bazel_format[@]}" --@@WORKSPACE@//:dry_run=False $files
+[[ $file_count -eq 0 ]] || "${bazel_format_file[@]}" --@@WORKSPACE@//:dry_run=False $files
 
-for arg in $files; do
+# format all header only libs
+[[ $header_file_count -eq 0 ]] || "${bazel_format[@]}" --@@WORKSPACE@//:dry_run=False $header_libs
+
+for arg in $(echo "$files" "$header_files"); do
     # fix file mode bits
     # https://github.com/bazelbuild/bazel/issues/2888
     chmod $(stat -c "%a" "$arg") "@BINDIR@${arg}.clang_format"
@@ -60,4 +85,5 @@ for arg in $files; do
 done
 
 # run format check to cache success
-"${bazel_format[@]}" $files
+[[ $file_count -eq 0 ]] || "${bazel_format_file[@]}" $files
+[[ $header_file_count -eq 0 ]] || "${bazel_format[@]}" $header_libs
